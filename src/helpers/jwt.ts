f496af7be1b5500f7a2a711ef62c9e922aab1bab
Estkhar Ansari <estkharansari123@gmail.com>
2024-07-22 13:13:37 +0530

    Admin boiler Plate

---
+++ src/helpers/jwt.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import jwt from 'jsonwebtoken'
+
+
+export interface IPayload{
+    _id:string
+}
+
+export const generate_token = (payload:IPayload): string => {
+   
+    const token = jwt.sign(
+		{
+			_id: payload._id,
+		},
+		process.env.JWT_SECRET,
+		{ expiresIn: process.env.JWT_EXPIRE }
+	)
+
+    return token
+}
